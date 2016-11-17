e9291a45f13ce5a8891985f0f63d21ec6072dd3c
JaeHo Song <thruthesky@gmail.com>
2016-11-17 23:50:22 +0900

    adding first import

---
+++ xbase-module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { IonicModule } from 'ionic-angular';
+import { Xbase } from './xbase';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+      IonicModule
+  ],
+  entryComponents: [
+  ],
+  providers: [ Xbase ]
+})
+export class XbaseModule {}
---
+++ xbase.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class Xbase {
+
+    constructor() {
+        console.log("Xbase::constrcutor");
+    }
+
+}
